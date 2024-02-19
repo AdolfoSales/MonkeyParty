adef72926566a6383dc92f58fbfecca46907daa5
AitorLegor <alegorburu192@gmail.com>
2024-02-19 12:21:19 +0100

    CrossroadsManualV01
    Cambios inacabados de crossroads para el tablero, jusnto antes de otro commit exprimental para comprobar cual funciona mejor

--- Assets/Scripts/Tablero/PlayerControllerV02.cs
+++ Assets/Scripts/Tablero/PlayerControllerV02.cs
@@ -2,19 +2,61 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerControllerV02 : MonoBehaviour
 {
-    
-    [SerializeField] GameObject[] _cells;
+    [SerializeField] GameObject[] _crossRoads;
+
+    [SerializeField] GameObject[] _roadA;
+    [SerializeField] GameObject[] _roadB;
+
+    [SerializeField] GameObject[] chosenPath;
 
 
     [SerializeField] int _currentCellIndex = 0;
 
     [SerializeField] float _moveSpeed = 5f;
 
+
+    [SerializeField] Button _crossRoads1Left, _crossRoads1Right;
+
+    bool _canMove = true;
+
     Vector3 targetPosition;
 
+    int newCrossRoadsIndex = 0;
+
+    private void Awake()
+    {
+        chosenPath = _roadA;
+    }
+
+
+    public void Update()
+    {
+        
+    }
+
+    private IEnumerator CrossRoadsCoroutine(int newCrossRoadsIndex)
+    {
+        Vector3 crossRoadsTarget = _crossRoads[newCrossRoadsIndex].transform.position + new Vector3(0, 1.1f, 0);
+
+        while (transform.position != crossRoadsTarget)
+        {
+            yield return null;
+        }
+
+        _canMove = false;
+        _crossRoads1Left.enabled = _crossRoads1Right.enabled = true;
+
+        
+    }
+    public void ChooseCrossRoads(GameObject[] pathToChoose)
+    {
+
+    }
+        
     public void RollD6()
     {
         // Implement your dice rolling logic here, for example:
@@ -22,16 +64,18 @@ public class PlayerControllerV02 : MonoBehaviour
 
         Move(rolledNumber);
     }
+    public void ChoosePath(GameObject[] path)
+    {
 
+        chosenPath = path;
+    }
 
     private void Move(int rolledNumber)
     {
         // Calculate the new cell index based on the rolled number
-        int newCellIndex = (_currentCellIndex + rolledNumber) % _cells.Length;
+        int newCellIndex = (_currentCellIndex + rolledNumber) % chosenPath.Length;
 
-        Vector3 targetPosition = _cells[newCellIndex].transform.position + new Vector3(0, 1.1f, 0);
-
-
+        Vector3 targetPosition = chosenPath[newCellIndex].transform.position + new Vector3(0, 1.1f, 0);
 
         StartCoroutine(MoveCoroutine(targetPosition, newCellIndex));
     }
@@ -39,19 +83,30 @@ public class PlayerControllerV02 : MonoBehaviour
 
     private IEnumerator MoveCoroutine(Vector3 targetPosition, int newCellIndex)
     {
-        while (transform.position != targetPosition)
+        while (transform.position != targetPosition && _canMove == true)
         {
-            int nextCellToGo = (_currentCellIndex + 1) % _cells.Length;
+            int nextCellToGo = (_currentCellIndex + 1) % chosenPath.Length;
 
-            Vector3 nextCellTarget = _cells[nextCellToGo].transform.position + new Vector3(0, 1.1f, 0);
+            Vector3 nextCellTarget = chosenPath[nextCellToGo].transform.position + new Vector3(0, 1.1f, 0);
 
             if (transform.position == nextCellTarget)
             {
+                if (transform.position == _crossRoads[newCrossRoadsIndex].transform.position)
+                {
+                    StartCoroutine(CrossRoadsCoroutine(newCrossRoadsIndex));
+                }
+
+                yield return StartCoroutine(CrossRoadsCoroutine(newCrossRoadsIndex));
                 _currentCellIndex++;
             }
 
             // Move towards the target position
-            transform.position = Vector3.MoveTowards(transform.position, nextCellTarget, _moveSpeed * Time.deltaTime);
+
+            if ( _canMove == true)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, nextCellTarget, _moveSpeed * Time.deltaTime);
+            }
+            
 
             yield return null; // Wait for the next frame
         }
@@ -59,6 +114,4 @@ public class PlayerControllerV02 : MonoBehaviour
         // Update the current cell index once the movement is complete
         _currentCellIndex = newCellIndex;
     }
-
-
 }
